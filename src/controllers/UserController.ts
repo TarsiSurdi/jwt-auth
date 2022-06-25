ff1c3e77001274ecc0db40b48ba6e3f27ba67b2f
Tarcísio Surdi <tarsisurdi@gmail.com>
2022-06-24 23:21:08 -0300

    Implemented basic controller for /users

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import prisma from "../database/connection";
+
+class UserController {
+  async index(req: Request, res: Response) {
+    const users = await prisma.user.findMany();
+
+    return res.json(users);
+  }
+
+  async create(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    const userExists = await prisma.user.findUnique({ where: { email } });
+
+    if (userExists) {
+      return res.sendStatus(409);
+    }
+
+    const user = await prisma.user.create({ data: { email, password } });
+
+    return res.json(user);
+  }
+}
+
+export default new UserController();
---
+++ src/database/connection.ts
@@ -0,0 +1,4 @@
+import { PrismaClient } from "@prisma/client";
+
+// Singleton
+export default new PrismaClient();
