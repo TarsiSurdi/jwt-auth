76fcd5e1f5cf070db5a4476dc79aeffd9bcf2d0a
Tarcísio Surdi <tarsisurdi@gmail.com>
2022-06-24 23:35:18 -0300

    Refactoring database connection

--- src/database/connection.ts
+++
@@ -1,4 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-// Singleton
-export default new PrismaClient();
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
+import { PrismaClient } from "@prisma/client";
 import express from "express";
 import routes from "./routes";
 
 const PORT = Number(process.env.PORT) || 3000;
 const HOSTNAME = process.env.HOST || "localhost";
+
+export const prisma = new PrismaClient();
 
 const app = express();
 
@@ -11,4 +14,12 @@ app.use(routes);
 
 app.listen(PORT, HOSTNAME, () => {
   console.log(`🔥 Server started at http://${HOSTNAME}:${PORT}`);
+  prisma
+    .$connect()
+    .then(() => {
+      console.log("📦 Succesfully connected to database!");
+    })
+    .catch((err) => {
+      console.error("🚫 Database connection error:\n" + err);
+    });
 });
