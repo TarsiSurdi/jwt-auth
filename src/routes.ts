777767d65ab51a4746cb0871c27eb4f878675718
Tarcísio Surdi <tarsisurdi@gmail.com>
2022-06-26 11:14:58 -0300

    Added protected and auth routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 
+import AuthMiddleware from "./middlewares/AuthMiddleware";
+
+import AuthController from "./controllers/AuthController";
 import UserController from "./controllers/UserController";
 
 const router = Router();
 
-router.get("/users", UserController.index);
+router.get("/users", AuthMiddleware, UserController.index);
 router.post("/users", UserController.create);
+
+router.post("/auth", AuthController.authenticate);
 
 export default router;
